cb76414cea6b1e8aef33c585bfe4437870090ca5
Alessio Quaresima <alequa@mpi.nl>
2022-03-02 16:05:04 +0100

    snnutils write/read encompass all strcts

--- src/connections/structs.jl
+++ src/connections/structs.jl
@@ -4,13 +4,6 @@ Return a sparse matrix with ρ density of non-null entries.
 """
 
 
-function sparser(matrix::Array,ρ::Real)
-    ρ == 0. && return zeros(size(matrix))
-    sparse = findall(x -> rand()>ρ, matrix)
-    matrix[sparse] .=0
-    return matrix
-end
-
 @with_kw struct Connections
     params::NamedTuple
     map::Vector{Vector{Symbol}}
