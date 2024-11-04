dcdec0abec3e9a07ecbc8feee44578b64d696bf9
galborsz <galborsz@gmail.com>
2024-11-04 08:56:34 +0100

    fixed accuracy calculation

--- src/analysis/performance.jl
+++ src/analysis/performance.jl
@@ -2,7 +2,7 @@ function evaluate(population, intervals::Vector{Vector{Float32}}, target::Symbol
     count = 0
     for time_interval in intervals
         interval_range = range(first(time_interval), stop=last(time_interval), length=length(time_interval))
-        firing_rates = Dict(w => SNN.average_firing_rate(population; interval=interval_range, pop=cells[w]) for w in keys(cells))
+        firing_rates = Dict(w => mean(SNN.average_firing_rate(population; interval=interval_range, pop=cells[w])) for w in keys(cells))
 
         if all(firing_rates[target] > firing_rates[w] for w in keys(cells) if w != target)
             count += 1
