39bff18dcd05e1e91310589d8b1cb70f2e24b08f
Alessio <alessio.quaresima@mpi.nl>
2024-11-26 14:25:20 +0100

    added updown params

--- src/models/models.jl
+++ src/models/models.jl
@@ -1,5 +1,6 @@
-include("quaresima2023.jl")
 include("quaresima2022.jl")
+include("quaresima_2024_updown.jl")
+include("quaresima2023.jl")
 include("duarte2019.jl")
 include("lkd2014.jl")
 include("connections.jl")
