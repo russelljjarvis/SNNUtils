afbe2a0941092a0861b746d534378eb4db793bf0
Alessio <alessio.quaresima@mpi.nl>
2024-11-28 17:32:39 +0100

    added compute weight function

--- src/analysis/performance.jl
+++ src/analysis/performance.jl
@@ -11,25 +11,55 @@ function evaluate(population, intervals::Vector{Vector{Float32}}, target::Symbol
     return count / length(intervals)
 end
 
-
-function compute_weight(pre_pop_cells, post_pop_cells, synapse)
-    Win = synapse.W
+function average_weight(pre_pop_cells::Vector{Int}, post_pop_cells::Vector{Int}, synapse::SNN.SpikingSynapse)
+    @unpack W = synapse
     rowptr = synapse.rowptr
     J = synapse.J  # Presynaptic neuron indices
     index = synapse.index 
-    all_weights = Float64[]  # Store weights for all filtered connections
-
+    all_weights = Float32[]  # Store weights for all filtered connections
     for neuron in post_pop_cells
         # Get the range in W for this postsynaptic neuron's incoming connections
         for st = rowptr[neuron]:(rowptr[neuron + 1] - 1)
             st = index[st]
             if (J[st] in pre_pop_cells)
-                push!(all_weights, Win[st])
+                push!(all_weights, W[st])
             end
         end
     end
-
     return mean(all_weights)
 end
 
-export evaluate, compute_weight
+function weights_indices(pre_pop_cells::Vector{Int}, post_pop_cells::Vector{Int}, synapse::SNN.SpikingSynapse)
+    rowptr = synapse.rowptr
+    J = synapse.J  # Presynaptic neuron indices
+    index = synapse.index 
+    indices = Int64[]  # Store weights for all filtered connections
+    for neuron in post_pop_cells
+        # Get the range in W for this postsynaptic neuron's incoming connections
+        for st = rowptr[neuron]:(rowptr[neuron + 1] - 1)
+            st = index[st]
+            if (J[st] in pre_pop_cells)
+                push!(indices, st)
+            end
+        end
+    end
+    return indices
+end
+
+function update_weight!(pre_pop_cells::Vector{Int}, post_pop_cells::Vector{Int}, synapse::SNN.SpikingSynapse)
+    @unpack W = synapse
+    rowptr = synapse.rowptr
+    J = synapse.J  # Presynaptic neuron indices
+    index = synapse.index 
+    for neuron in post_pop_cells
+        # Get the range in W for this postsynaptic neuron's incoming connections
+        for st = rowptr[neuron]:(rowptr[neuron + 1] - 1)
+            st = index[st]
+            if (J[st] in pre_pop_cells)
+                W[st] *= 1.2
+            end
+        end
+    end
+end
+
+export evaluate, average_weight, update_weight!, weights_indices
