b631ab30fe64622d8b1f8600e4dd869113a20920
Alessio <alessio.quaresima@mpi.nl>
2024-12-27 13:46:35 +0100

    compute kei analytical

--- src/stimuli/base.jl
+++ src/stimuli/base.jl
@@ -1,5 +1,6 @@
 include("sequence/sequence.jl")
 include("sequence/sequence_generators.jl")
 include("balance_EI/load.jl")
+include("balance_EI/compute_kei.jl")
 include("bioseq/import_bioseq.jl")
 
